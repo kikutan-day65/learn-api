33e3082d64144b32e8e27bf2d1100f2ceb8582db
Haruto Mori <baystarsman@outlook.jp>
2023-10-21 17:24:06 +0900

    create a POST request

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 
 app = FastAPI()
 
 
+# GET request
 @app.get("/")
 def root():
     return {"message": "Welcome to my API"}
@@ -12,3 +14,10 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+
+# POST request
+@app.post("/createposts")
+def create_posts(payLoad: dict = Body(...)): # data stored in payLoad
+    print(payLoad)
+    return {"new_post": f"title: {payLoad['title']} content: {payLoad['content']}"}
